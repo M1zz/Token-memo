1711bcd77fde68c304d89911535705a24038ee81
hyunho <mizzking75@gmail.com>
2023-05-14 13:46:08 +0900

    Initial Commit

---
+++ Token memo/Token_memoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Token_memoApp.swift
+//  Token memo
+//
+//  Created by hyunho lee on 2023/05/14.
+//
+
+import SwiftUI
+
+@main
+struct Token_memoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
