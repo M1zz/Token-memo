569193f2ea24a99d01f673d247142acbc8e053a8
Jihoonahn <jihoonahn.dev@gmail.com>
2023-05-23 01:20:52 +0900

    Make Builder

--- TokenKeyboard/Utility/Foundation/Builder/Builder.swift
+++ TokenKeyboard/Utility/Foundation/Builder/Builder.swift
@@ -1,8 +1,26 @@
-//
-//  Builder.swift
-//  TokenKeyboard
-//
-//  Created by Jihoonahn on 2023/05/23.
-//
-
+/// https://github.com/pelagornis/Builder
 import Foundation
+import UIKit
+
+/// Protocol required to support builder patterns.
+public protocol BuilderCompatible {
+    associatedtype Base
+    func builder() -> Builder<Base>
+}
+
+extension BuilderCompatible {
+    public func builder() -> Builder<Self> {
+        Builder(self)
+    }
+}
+
+extension UIEdgeInsets: BuilderCompatible {}
+extension UIOffset: BuilderCompatible {}
+extension UIRectEdge: BuilderCompatible {}
+
+extension Array: BuilderCompatible {}
+extension Dictionary: BuilderCompatible {}
+extension Set: BuilderCompatible {}
+extension JSONDecoder: BuilderCompatible {}
+extension JSONEncoder: BuilderCompatible {}
+extension NSObject: BuilderCompatible {}
--- TokenKeyboard/Utility/Foundation/Builder/BuilderCompatible.swift
+++ TokenKeyboard/Utility/Foundation/Builder/BuilderCompatible.swift
@@ -1,8 +1,66 @@
-//
-//  BuilderCompatible.swift
-//  TokenKeyboard
-//
-//  Created by Jihoonahn on 2023/05/23.
-//
-
+/// https://github.com/pelagornis/Builder
 import Foundation
+
+/// Builder Features.
+@dynamicMemberLookup
+public struct Builder<Base> {
+    /// Base
+    private let base: Base
+
+    /// Initalizer
+    public init(_ base: Base) {
+        self.base = base
+    }
+
+    /// Readable and flexible data settings
+    ///
+    ///```swift
+    /// let view = UIView()
+    ///           .builder()
+    ///           .backgroundColor(.red)
+    ///           .build()
+    ///```
+    ///
+    /// - Parameter keyPath: The key path of the property to set.
+    /// - Returns: The object being configured.
+    public subscript<Value>(
+        dynamicMember keyPath: WritableKeyPath<Base, Value>
+    ) -> (Value) -> Builder<Base> {
+        return { [base] value in
+            var object = base
+            object[keyPath: keyPath] = value
+            return Builder(object)
+        }
+    }
+
+    /// Execute closure using objects as arguments within a builder pattern
+    ///
+    /// ```swift
+    /// let view = UIView()
+    ///               .builder()
+    ///               .backgroundColor(.red)
+    ///               .with {
+    ///                   $0.layer.cornerRadius = 15
+    ///                }
+    ///                .build()
+    /// ```
+    ///
+    /// - Parameter block: The closure to execute.
+    /// - Returns: The object being configured.
+    public func with(_ block: (inout Base) -> Void) -> Builder<Base> {
+        Builder(self.with(block))
+    }
+
+    /// Build is responsible for creating objects.
+    public func build() -> Base {
+        return self.base
+    }
+}
+
+extension Builder {
+    public func with(_ block: (inout Base) -> Void) -> Base {
+        var object = self.base
+        block(&object)
+        return object
+    }
+}
