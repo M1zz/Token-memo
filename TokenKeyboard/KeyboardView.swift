098ea06773964977ee16abf1b1b213f31b88bb58
hyunho <mizzking75@gmail.com>
2023-10-04 00:45:14 +0900

    fix to swiftUI

---
+++ TokenKeyboard/KeyboardView.swift
@@ -0,0 +1,18 @@
+//
+//  ClipListView.swift
+//  TokenKeyboard
+//
+//  Created by hyunho lee on 2023/10/03.
+//
+
+import SwiftUI
+
+struct ClipListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClipListView()
+}
