43b94ef945a6a4332bc60c4fa1ebaa4eab1787df
hyunho <mizzking75@gmail.com>
2023-10-05 23:47:14 +0900

    add mail

--- Token memo/Screens/SettingView.swift
+++ Token memo/Screens/SettingView.swift
@@ -14,6 +14,10 @@ struct SettingView: View {
     
     var body: some View {
         List {
+            NavigationLink(destination: TutorialView()) {
+                Text("웹뷰")
+            }
+            
             NavigationLink(destination: CopyTutorialView()) {
                 Text("복사 사용방법 튜토리얼")
             }
@@ -32,6 +36,10 @@ struct SettingView: View {
             
             NavigationLink(destination: ReviewWriteView()) {
                 Text("리뷰 및 평점 매기기")
+            }
+            
+            NavigationLink(destination: ContactView()) {
+                Text("개발자에게 연락하기")
             }
         }
     }
@@ -77,7 +85,73 @@ struct ReviewWriteView: View {
     }
 }
 
+struct TutorialView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        VStack {
+            Button("Open Web Page") {
+                
+            }
+            .onAppear(perform: {
+                dismiss()
 
+                if let url = URL(string: "https://leeo75.notion.site/Swift-Coding-Class-3a037e69b3ca484d88b369f0d24030ff?pvs=4") {
+                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                }
+            })
+        }
+    }
+}
+
+struct ContactView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        VStack {
+            Button("Send Email") {
+                
+            }
+            .onAppear(perform: {
+                dismiss()
+
+                EmailController.shared.sendEmail(subject: "클립 키보드에 관해 문의드릴 것이 있습니다", body: "안녕하세요 저는 클립키보드의 사용자입니다.", to: "clipkeyboard@gmail.com")
+            })
+        }
+    }
+}
+
+import MessageUI
+class EmailController: NSObject, MFMailComposeViewControllerDelegate {
+    public static let shared = EmailController()
+    private override init() { }
+    
+    func sendEmail(subject:String, body:String, to:String){
+        // Check if the device is able to send emails
+        if !MFMailComposeViewController.canSendMail() {
+           print("This device cannot send emails.")
+           return
+        }
+        // Create the email composer
+        let mailComposer = MFMailComposeViewController()
+        mailComposer.mailComposeDelegate = self
+        mailComposer.setToRecipients([to])
+        mailComposer.setSubject(subject)
+        mailComposer.setMessageBody(body, isHTML: false)
+        EmailController.getRootViewController()?.present(mailComposer, animated: true, completion: nil)
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        EmailController.getRootViewController()?.dismiss(animated: true, completion: nil)
+    }
+    
+    static func getRootViewController() -> UIViewController? {
+        // In SwiftUI 2.0
+        UIApplication.shared.windows.first?.rootViewController
+    }
+}
 
 struct SettingView_Previews: PreviewProvider {
     static var previews: some View {
