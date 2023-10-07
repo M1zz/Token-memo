61fdd967ded92f53a0a574a8b47cd121b81c5382
hyunho <mizzking75@gmail.com>
2023-10-07 23:49:13 +0900

    add keyboard color

--- TokenKeyboard/KeyboardView.swift
+++ TokenKeyboard/KeyboardView.swift
@@ -8,36 +8,43 @@
 import SwiftUI
 
 struct KeyboardView: View {
-//    var hashTagArray: [String] = ["#Lorem", "#Ipsum", "#dolor", "#consectetur", "#adipiscing", "#elit", "#Nam", "#semper", "#sit", "#amet", "#ut", "#eleifend", "#Cras"]
     
-    private var gridItemLayout = [GridItem(.adaptive(minimum: 100))]
+    private var gridItemLayout = [GridItem(.adaptive(minimum: 130))]
     
     var body: some View {
-        ScrollView{
-            LazyVGrid(columns: gridItemLayout , spacing: 5)  {
-                ForEach(clipKey.indices, id:\.self) { i in
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 10)
-                            .foregroundColor(.white)
-                            .shadow(color: Color.black.opacity(0.5), radius: 1, y: 2)
+        ZStack {
+            Color("KeyboardBackground").ignoresSafeArea()
+            ScrollView {
+                LazyVGrid(columns: gridItemLayout , spacing: 5)  {
+                    ForEach(clipKey.indices, id:\.self) { i in
+                        
                         HStack {
                             Button {
                                 UIImpactFeedbackGenerator().impactOccurred()
                                 NotificationCenter.default.post(name: NSNotification.Name(rawValue: "addTextEntry"), object: clipValue[i])
                             } label: {
-                                Text(clipKey[i])
-                                    .foregroundColor(.black)
-                                    .lineLimit(1)
-                                    .padding([.top, .bottom], 12)
-                                    .padding([.leading, .trailing])
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .foregroundColor(Color("KeyColor"))
+                                        .shadow(color: Color.black.opacity(0.5), radius: 1, y: 2)
+                                    Text(clipKey[i])
+                                        .foregroundStyle(Color(uiColor: .label))
+                                        .lineLimit(1)
+                                        .padding([.top, .bottom], 12)
+                                        .padding([.leading, .trailing])
+                                        .bold()
+                                }
+                                
+                                
+                                
                             }
                         }
                     }
                 }
+                .padding()
             }
-            .padding()
+            .frame(width: UIScreen.main.bounds.size.width)
         }
-        .frame(width: UIScreen.main.bounds.size.width)
     }
 }
 
